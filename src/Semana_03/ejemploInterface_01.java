0f503ca32a83e1858610b7418717822fb52ec512
Arkan <Arkan@10.com>
2026-04-30 10:27:08 -0500

    entregable 1

---
+++ src/Semana_03/ejemploInterface_01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana_03;
+
+/**
+ *
+ * @author LENOVO
+ */
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+
+public class ejemploInterface_01 {
+    public static void main(String[] args){
+        
+        
+    }
+}
